fb4b429cc9b27df77dbcc31dcc86b51f5f0af28b
Paul Barrett <pmbarrett314@gmail.com>
2020-09-18 16:00:36 -0500

    added long description to setup.py

--- mtga_follower/setup.py
+++ mtga_follower/setup.py
@@ -1,11 +1,13 @@
 import setuptools
 
 setuptools.setup(
-    name="seventeenlands",
+    name="test-seventeenlands",
     version="0.2.0",
     author="Example Author",
     author_email="author@example.com",
     description="Script to upload MTG Arena data to 17lands",
+    long_description="Long description",
+    long_description_content_type="text/plain",
     url="https://github.com/rconroy293/mtga-log-client",
     packages=setuptools.find_packages(),
     classifiers=[
