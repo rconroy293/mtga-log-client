09824f6e2773df7251402baab4b71fa56caacacc
Robert Conroy <rconroy293@gmail.com>
2020-12-01 20:49:37 -0700

    Bump pypi version to match client version

--- src/python/setup.py
+++ src/python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="seventeenlands",
-    version="0.0.5",
+    version="0.1.15",
     author="Robert Conroy",
     author_email="seventeenlands@gmail.com",
     description="Utility to upload MTG Arena data to 17Lands.com",
