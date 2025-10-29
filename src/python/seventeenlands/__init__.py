a9210f2c2a12f314906ad9a50b105e738b9b8d26
Robert Conroy <7552403+rconroy293@users.noreply.github.com>
2025-10-28 21:02:59 -0600

    Bump Python version

--- src/python/seventeenlands/__init__.py
+++ src/python/seventeenlands/__init__.py
@@ -1,3 +1,3 @@
 """17Lands MTGA Log Client - Upload MTG Arena data to 17Lands.com"""
 
-__version__ = "0.1.43"
+__version__ = "0.1.44"
--- src/python/seventeenlands/mtga_follower.py
+++ src/python/seventeenlands/mtga_follower.py
@@ -36,7 +36,7 @@ import seventeenlands.logging_utils
 
 logger = seventeenlands.logging_utils.get_logger("17Lands")
 
-CLIENT_VERSION = "0.1.43.p"
+CLIENT_VERSION = "0.1.44.p"
 
 UPDATE_CHECK_INTERVAL = datetime.timedelta(hours=1)
 UPDATE_PROMPT_FREQUENCY = 24
